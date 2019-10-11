d5c3634acc05491a4f1ea86bd7ebfba0565607e0
Serena Li <serena.li.usa@gmail.com>
2019-10-10 17:17:40 -0700

    Finish writing pneumatic code

--- src/main/java/frc/mechs/DecapitationMech.java
+++ src/main/java/frc/mechs/DecapitationMech.java
@@ -8,31 +8,27 @@ import edu.wpi.first.wpilibj.Solenoid;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
-
 public class DecapitationMech extends Mech {
 
     private Solenoid sol;
 
     private static final double SPEED = 0.1;
     private int direction;
-    private TalonSRX mechTal;
+    // private TalonSRX mechTal;
 
-    public DecapitationMech(){
+    public DecapitationMech() {
         sol = new Solenoid(Config.getInt("sol"));
-        mechTal = new TalonSRX(Config.getInt("mechTal"));
+        // mechTal = new TalonSRX(Config.getInt("mechTal"));
     }
 
     public void loop() throws InterruptedException {
-
-        //TODO: Here is an example of how to control different mechanisms
-
         // Pneumatics
         sol.set(true);
         Thread.sleep(3000);
         sol.set(false);
-
+        Thread.sleep(3000);
         // Motors
-        direction = (Math.random() > 0.5) ? -1 : 1;
-        mechTal.set(ControlMode.PercentOutput, SPEED * direction);
+        // direction = (Math.random() > 0.5) ? -1 : 1;
+        // mechTal.set(ControlMode.PercentOutput, SPEED * direction);
     }
-} 
+}
